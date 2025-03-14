ffbba7296804e190ad992fae41baa96cbad8591f
Muhammad Ali2 <muhammad.ali2@fdmgroup.com>
2025-03-14 16:29:44 +0000

    Revert "Merge branch 'feature/Set-up-testing' into 'Development'"
    This reverts merge request !27

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from app.routes import api_bp
 from .exts import db, migrate, jwt
 from .config import config_dict
@@ -8,11 +8,11 @@ from .config import Config
 import os
 from flask_mail import Mail
 
-def create_app(config_name="testing"):
+def create_app(config_name="development"):
     app = Flask(__name__)
     
     # Load the correct config
-    config_class = config_dict.get(config_name, "testing")
+    config_class = config_dict.get(config_name, "development")
     app.config.from_object(config_class)
 
     # Debugging: Print current config/checking to see which server i am using
