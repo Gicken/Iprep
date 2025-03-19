f3016396d88ada92b39389cd98622546430b021f
Jonathan Lee <jonathan.lee1@fdmgroup.com>
2025-03-19 11:48:13 +0000

    removed uneeded import

--- client/src/pages/CVManager.jsx
+++ client/src/pages/CVManager.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useOutletContext } from "react-router-dom";
 import { API_ENDPOINTS } from '../utils/constants';
-import { set } from 'express/lib/application';
 
 const CVManager = () => {
   const [cvs, setCvs] = useState([]);
