d4d683ac1d18641c4fac1c84ff8246862e8cda10
Jonathan Lee1 <jonathan.lee1@fdmgroup.com>
2025-03-19 16:08:49 +0000

    Update config.py

--- server/app/config.py
+++ server/app/config.py
@@ -7,8 +7,6 @@ load_dotenv()
 
 class Config:
     """Base configuration"""
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
-
     SECRET_KEY = os.getenv("SECRET_KEY", "your_default_secret_key")
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
