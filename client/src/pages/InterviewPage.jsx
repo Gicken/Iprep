32e534b260560b1febca46b8f22b6704f192be71
shubhra <shubhramishra03@gmail.com>
2025-03-31 14:27:07 +0100

    Save changes before switching branches

--- client/src/pages/InterviewPage.jsx
+++ client/src/pages/InterviewPage.jsx
@@ -1,4 +1,4 @@
-// InterviewPage.js
+
 import React, { useState, useEffect } from "react";
 import { useOutletContext } from "react-router-dom";
 import VoiceRecorder from "../components/interview/VoiceRecorder";
